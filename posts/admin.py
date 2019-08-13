564b1eadaf9df46fe3018938e7cc51e9f8dbae82
adoleba <ania.doleba@wp.pl>
2019-08-13 16:32:49 +0200

    admin - configuration

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from posts.models import PostCategory, Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published', 'status', 'author')
+    list_filter = ('status',)
+    search_fields = ('title', 'body')
+    ordering = ['published']
+
+
+admin.site.register(PostCategory)
+admin.site.register(Post, PostAdmin)
