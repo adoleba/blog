9d3bacef480082d669cd37afbd58a746c2ba64d5
adoleba <ania.doleba@wp.pl>
2019-08-14 13:49:27 +0200

    copied staticfiles, base template

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from posts.views import index, post_detail, posts_from_category
 app_name = 'post'
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('<int:year>/<int:month>/<int:day>/<slug>/', post_detail, name='post_detail'),
 
 ]
