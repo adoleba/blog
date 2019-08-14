815beee7571220d4d7be0a7222e67589c13d0911
adoleba <ania.doleba@wp.pl>
2019-08-14 14:35:13 +0200

    solved problem with media path

--- blog/settings.py
+++ blog/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = ('media',)
+MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = ('static',)
