003a4b63c602cc635dd0c525af770b1111a3bdb5
adoleba <ania.doleba@wp.pl>
2019-08-21 13:19:20 +0200

    in user_profile template possibility to search all user posts

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from users.views import user_profile
+from users.views import user_profile, user_posts
 
 app_name = 'users'
 
 urlpatterns = [
-    path('<username>/', user_profile, name='user_profile'),
+    path('<user>/', user_profile, name='user_profile'),
+    path('<user>/posts/', user_posts, name='user_posts'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
+
+from posts.models import Post
 from users.models import UserProfile
 
 
-def user_profile(request, username):
-    userprofile = get_object_or_404(User, username=request.user)
+def user_profile(request, user):
+    userprofile = UserProfile.objects.filter(user__username=user)
+    for field in userprofile:
+        about = field.about_user
+        intro = field.intro_user
+        photo = field.photo
+        user = field.user
     return render(request, 'users/user_profile.html',
-                  {'userprofile': userprofile})
+                  {'userprofile': userprofile, 'about': about, 'intro': intro, 'photo': photo, 'user': user})
+
+
+def user_posts(request, user):
+    userprofile = UserProfile.objects.filter(user__username=user)
+    for field in userprofile:
+        user = field.user
+        photo = field.photo
+    posts = Post.objects.filter(author=user)
+    return render(request, 'users/user_posts.html', {'posts': posts, 'user': user, 'photo': photo})
