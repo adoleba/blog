7dd89babdfea53820cd6861407331d7872b02b07
adoleba <ania.doleba@wp.pl>
2019-08-21 15:26:12 +0200

    added field photo to categories

--- categories/models.py
+++ categories/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 class Category(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField(max_length=20, unique=True, null=True)
+    photo = models.ImageField(upload_to='images/%Y/%m/%d', null=True)
 
     def __str__(self):
         return self.name
