8091bde6be0dd640b097c8c0113fec9897636016
adoleba <ania.doleba@wp.pl>
2019-08-22 14:44:34 +0200

    new group - authors, only superusers can change permissions

--- posts/admin.py
+++ posts/admin.py
@@ -8,6 +8,17 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'body')
     ordering = ['published']
     prepopulated_fields = {'slug': ('title',)}
+    exclude = ['author', ]
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
 
 
 admin.site.register(Post, PostAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
+from django.forms import forms, ModelMultipleChoiceField
+
 from users.models import UserProfile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
@@ -10,6 +12,29 @@ class UserProfileInline(admin.StackedInline):
 
 class UserProfileAdmin(BaseUserAdmin):
     inlines = (UserProfileInline,)
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = list(super().get_fieldsets(request, obj))
+        if request.user.is_superuser:
+            fieldsets.append(
+                ('Permissions', {
+                    'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+                }))
+        return fieldsets
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(username=request.user)
+
+
 
 
 admin.site.unregister(User)
