3765e38c7d8cd99e9570470b17593133dced4015
adoleba <ania.doleba@wp.pl>
2019-08-26 12:04:39 +0200

    contact template with static contact form

--- contact/views.py
+++ contact/views.py
@@ -4,5 +4,5 @@ from contact.models import ContactPage
 
 
 def contact(request):
-    contact = get_object_or_404(ContactPage)
+    contact = get_object_or_404(ContactPage, pk=1)
     return render(request, 'contact/contact.html', {'contact': contact})
