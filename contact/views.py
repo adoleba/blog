1f95f567b2e5ef23f8128e60120158ad57ef016f
adoleba <ania.doleba@wp.pl>
2019-08-26 12:51:30 +0200

    Contact form - class and template

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
+from contact.forms import ContactForm
 from contact.models import ContactPage
 
 
 def contact(request):
     contact = get_object_or_404(ContactPage, pk=1)
-    return render(request, 'contact/contact.html', {'contact': contact})
+    form = ContactForm()
+    return render(request, 'contact/contact.html', {'contact': contact, 'form': form})
