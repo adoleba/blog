fb401cb39f0b4eba961675f979605f0ec0c52752
adoleba <ania.doleba@wp.pl>
2020-04-15 11:55:39 +0200

    settings for static

--- blog/settings.py
+++ blog/settings.py
@@ -133,8 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = root('staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('static',)
+STATICFILES_DIRS = [root('static')]
+
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
