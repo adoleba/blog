9ce17150a973129cb43c4fd37c0011a07613c707
adoleba <ania.doleba@wp.pl>
2020-04-15 11:09:14 +0200

    logging django errors

--- blog/settings.py
+++ blog/settings.py
@@ -173,3 +173,37 @@ AUTH_USER_MODEL = 'users.User'
 
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']  # delete if will be postgres
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', ],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
